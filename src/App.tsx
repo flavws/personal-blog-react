914a5e7bea6a50ebabda6870857addaf6f97fe67
flavws <persona.fmaria@gmail.com>
2023-05-19 07:40:21 -0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-import { useState } from 'react';
-import './App.css';
-import Navbar from './components/statics/navbar/Navbar';
-import Footer from './components/statics/footer/Footer';
-import Home from './pages/home/Home';
+import { useState } from "react";
 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/statics/navbar/Navbar";
+import Footer from "./components/statics/footer/Footer";
+import Home from "./pages/home/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
+    <Router>
       <Navbar />
-      <Home />
+      <div style={{ minHeight: "100vh" }}>
+        <Routes>
+          <Route path="/home" element={<Home />}/>
+        </Routes>
+      </div>
       <Footer />
-    </>
+    </Router>
   );
 }
 
-export default App
+export default App;
